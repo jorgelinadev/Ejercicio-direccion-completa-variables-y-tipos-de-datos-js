3397467dc0287b2457a5a4d363b84a1c4b211485
jorgelinadev <jorgelinalberti@gmail.com>
2022-07-08 09:04:57 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,12 @@
+//alert ("Mi archivo está bien linkeado");
+
+const calle = prompt ('Ingrese su calle');
+const numero = prompt ('Ingrese su número');
+const departamento = prompt ('Ingrese su departamento');
+const codigoPostal = prompt ('Ingrese su código postal');
+const ciudad = prompt ('Ingrese su ciudad');
+const pais = prompt ('Ingrese su país');
+
+alert ('La dirección que usted ha ingresado es:  Calle:' + calle +'; Número: '+ numero + ';  Departamento: ' + departamento + ';  Código Postal:  '+ codigoPostal +';  Ciudad:  '+ ciudad + '; País:  '+ pais + '.');
+
+console.log (alert);
